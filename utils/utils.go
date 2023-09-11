a90afeba60e572197ba74965a7c12dd5645bfc9a
LiAng <L2545721422@163.com>
2023-09-11 14:24:47 +0800

    add GenerateRandomString

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	crypto_rand "crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"math/rand"
 	"net/url"
@@ -27,4 +29,23 @@ func RandomSleep(minSeconds, maxSeconds int) {
 	fmt.Printf("Sleeping for %f seconds...\n", randomSeconds)
 	time.Sleep(duration)
 	fmt.Println("Done sleeping!")
+}
+
+// 生成指定长度的随机字符串
+func GenerateRandomString(length int) (string, error) {
+	// 计算生成的字节数
+	byteLength := (length * 6) / 8 // base64 编码后，每个字符占 6 位，8 位字节可以编码为 6 位字符
+
+	// 生成随机字节
+	randomBytes := make([]byte, byteLength)
+	_, err := crypto_rand.Read(randomBytes)
+	if err != nil {
+		return "", err
+	}
+
+	// 使用 base64 编码生成的随机字节
+	randomString := base64.URLEncoding.EncodeToString(randomBytes)
+
+	// 截取指定长度的字符串
+	return randomString[:length], nil
 }
